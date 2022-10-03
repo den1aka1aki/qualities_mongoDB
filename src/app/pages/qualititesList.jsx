b7a24c52219f1136768e6ccf9b28b0a79c1795d8
den1aka1aki <lavrov.den@gmail.com>
2022-10-03 06:08:33 +0200

    workInProgress_new_project_mongoDB

--- src/app/pages/qualititesList.jsx
+++ src/app/pages/qualititesList.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useHistory } from "react-router-dom";
 import QualitiesTable from "../components/ui/qualitiesTable";
+import axios from "axios";
 
 const QualitiesListPage = () => {
     const history = useHistory();
+    const [qualities, setQualities] = useState([]);
+
+    useEffect(async() => {
+        const {data} = await axios
+            .get('http://localhost:4000/api/v1/quality')
+        setQualities(data.content)
+    }, [])
+
     const handleEdit = (param) => {
         console.log(param);
         history.push(`/edit/${param}`);
@@ -17,7 +26,7 @@ const QualitiesListPage = () => {
             <QualitiesTable
                 onDelete={handleDelete}
                 onEdit={handleEdit}
-                data={[]}
+                data={qualities}
             />
         </>
     );
